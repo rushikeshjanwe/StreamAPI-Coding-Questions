b73d7426a255f7d0baadcfb17b37d7b8a9e86e57
Rushikesh Janwe <90002061+rushikeshjanwe@users.noreply.github.com>
2026-04-01 19:02:56 +0530

    Update StringUppercase.java

--- src/com/example/StringUppercase.java
+++ src/com/example/StringUppercase.java
@@ -9,6 +9,8 @@ public class StringUppercase {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+
 		List<String> list=Arrays.asList("Roy","Tom","Jerry");
 		
 		List<String> result=list.stream()
