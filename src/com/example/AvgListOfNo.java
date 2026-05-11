7e52995be16d03304c6a1c55d266a09582e5432c
Rushikesh Janwe <90002061+rushikeshjanwe@users.noreply.github.com>
2026-05-11 19:15:35 +0530

    Update AvgListOfNo.java

--- src/com/example/AvgListOfNo.java
+++ src/com/example/AvgListOfNo.java
@@ -14,7 +14,6 @@ public class AvgListOfNo {
 		
 		System.out.println(result);
 		
-		
 		double result2=numbers.stream().filter(n->n%2==0).mapToInt(i->i).average().orElse(0.0);
 		
 		System.out.println(result2);
