bdf1f306177dfbf24ccec02b9942d98149870b36
Rushikesh Janwe <90002061+rushikeshjanwe@users.noreply.github.com>
2026-05-16 19:00:38 +0530

    Update FindElementGreaterBothNeighbor.java

--- src/com/example/FindElementGreaterBothNeighbor.java
+++ src/com/example/FindElementGreaterBothNeighbor.java
@@ -7,6 +7,7 @@ public class FindElementGreaterBothNeighbor {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] arr = {10, 20, 15, 25, 30, 20, 50};
+
 		
 		IntStream.range(1, arr.length-1)
 				 .filter(i -> arr[i] > arr[i-1] && arr[i] > arr[i+1])
