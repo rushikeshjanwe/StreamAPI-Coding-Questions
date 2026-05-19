a95902bf96d6ce800c44ff58dc2981c06f169381
Rushikesh Janwe <90002061+rushikeshjanwe@users.noreply.github.com>
2026-05-19 19:22:05 +0530

    Update AvgListOfNo.java

--- src/com/example/AvgListOfNo.java
+++ src/com/example/AvgListOfNo.java
@@ -9,7 +9,6 @@ public class AvgListOfNo {
 		// TODO Auto-generated method stub
 		
 		List<Integer> numbers = Arrays.asList(10, 20, 30, 40);
-
 		
 		double result=numbers.stream().mapToInt(Integer::intValue).average().orElse(0.0);
 		
